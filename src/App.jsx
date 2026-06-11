10ccdb60eaffec5513d87b2afc5d6633c84f0362
anjul3699 <anjulsam@gmail.com>
2026-06-11 18:53:26 +0200

    fix files

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,24 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Portfolio from './Components/Portfolio/Portfolio'
 import Contact from './Components/Contact/Contact'
-import { useState } from 'react'
-
 
 function App() {
-
-  const scrollToSection = (page) =>{
+  const scrollToSection = (page) => {
     const id = page.toLowerCase()
     const el = document.getElementById(id)
-    if (el) el.scrollIntoView({behavior: 'smooth'})
+    if (el) el.scrollIntoView({ behavior: 'smooth' })
   }
-  
 
   return (
     <div className="app-layout">
-      <Sidebar  setActivePage={scrollToSection} />
+      <Sidebar setActivePage={scrollToSection} />
       <main className='main-content'>
-      <section id='home'> <Hero/> </section>
-      <section id='about'> <About/> </section>
-      <section id='portfolio'> <Portfolio/> </section>
-      <section id='contact'><Contact/></section>
-      
-        
+        <section id='home'>      <Hero />      </section>
+        <section id='about'>     <About />     </section>
+        <section id='portfolio'> <Portfolio /> </section>
+        <section id='contact'>   <Contact />   </section>
       </main>
     </div>
   )
 }
-
 export default App
